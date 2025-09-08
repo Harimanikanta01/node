46ada9e1578a0f80ab8a792f3cfb99163c933129
Hari manikanta <punugulahari1@gmail.com>
2025-09-08 21:59:03 +0530

    main.js

--- main.js
+++ main.js
@@ -84,9 +84,11 @@ app.get("/", async (req, res) => {
 });
 const port = process.env.PORT || 4000;
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on port ${port}`);
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
 });
+
+
 
 
 
